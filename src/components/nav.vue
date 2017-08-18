37fbe70331ffe172cb43ce43d23cc69e9728767a
小小杨 <yanrome@users.noreply.github.com>
2017-08-18 15:43:05 +0800

    yang
    new

---
+++ src/components/nav.vue
@@ -0,0 +1,22 @@
+ <style lang="sass" scope>
+
+</style>
+<template>
+
+</template>
+
+<script>
+    export default {
+        data(){
+            return {}
+        },
+        computed: {},
+        created: function () {
+        },
+        mounted: function () {
+        },
+        methods: {}
+    }
+</script>
+
+
